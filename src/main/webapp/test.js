8e3575d2a738a9459c5159c82614db83ca6fd27e
MalgKa <kasprzak.malg@gmail.com>
2023-10-11 19:30:06 +0200

    js for tooltip

--- src/main/webapp/test.js
+++ src/main/webapp/test.js
@@ -1,4 +1,7 @@
 const timestampElements = document.querySelectorAll('.timestamp');
+
+// tooltip
+const tooltips = document.querySelectorAll('.tt')
 timestampElements.forEach((element) => {
     const timestamp = new Date(element.textContent);
     console.log(timestamp)
@@ -11,8 +14,14 @@ timestampElements.forEach((element) => {
     console.log(statusElement)
 
 
-    if (timeDifference >= 2 * 60 * 1000 && (statusElement.textContent === "new" || statusElement.textContent === "in progress")) {
+    if (timeDifference >= 40 * 60 * 1000 && (statusElement.textContent === "new" || statusElement.textContent === "in progress")) {
         element.style.color = 'red';
         element.style.fontWeight = 'bold'
     }
+
+// tooltip
+    tooltips.forEach(t=>{
+        new bootstrap.Tooltip(t)
+    })
+
 });
